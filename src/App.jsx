255461ef2763b0527513e0b151690410663ae924
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-19 22:20:12 +0330

    import BroweserRouter,Routes,Route from react-router-dom and map on routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import routes from "./routes";
 
 function App() {
-
+  console.log(routes);
   return (
     <div className="App">
-  
-
+      <BrowserRouter>
+        <Routes>
+          {routes.map((route) => (
+            <Route key={route.id} {...route} />
+          ))}
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
