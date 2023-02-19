5c862311bccab4d1952a106614f20aa3ed1f3c3e
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-19 22:21:47 +0330

    add pages folder

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../layout/Layout";
+
+const HomePage = () => {
+  return (
+    <Layout>
+      home
+
+    </Layout>
+  );
+};
+
+export default HomePage;
